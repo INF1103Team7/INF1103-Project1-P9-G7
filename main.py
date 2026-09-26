0a812e93a88f0be74392b8a67e9af8d3652fa84e
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-26 23:08:09 +0800

    Added skeleton structure for main.py to run the CLI and call other modules.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import io_manager
+import ai_manager
+import data_manager
+import logic_manager
 
+def main():
+	user_input = io_manager.run_cli()
+	return
 
 if __name__ == "__main__":
-	io_manager.run_cli()
+    main()
